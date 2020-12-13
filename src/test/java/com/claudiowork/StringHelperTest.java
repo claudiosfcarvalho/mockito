ad3ad2e685630b3170eb5d9e01a756d2bb0c8984
Claudio <claudiosfcarvalho@outlook.com>
2020-12-13 19:08:57 -0300

    init

---
+++ src/test/java/com/claudiowork/StringHelperTest.java
@@ -0,0 +1,19 @@
+package com.claudiowork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void testTruncateAInFirst2Positions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame() {
+		fail("Not yet implemented");
+	}
+
+}
